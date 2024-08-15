79a526b5a2498228c90be7686ab237865e7794b8
Keidi Teixeira Barbieri <keidifofa@gmail.com>
2024-08-15 01:10:59 -0300

    denovo

--- app/areas-conhecimento/page.tsx
+++ app/areas-conhecimento/page.tsx
@@ -1,114 +1,12 @@
-'use client'
+// app/areas-conhecimento/page.tsx
 
-import { useSearchParams } from 'next/navigation';
-import { Button } from '@/components/Button';
-import { Container } from '@/components/Container';
-import React, { Suspense } from 'react';
-
-function GetHumanas({ trimestre }: { trimestre: string }) {
-  return (
-    <div className="rounded-2xl border border-sunglow bg-space-cadet bg-topography-sunglow p-6">
-      <Button
-        href={`/areas-conhecimento/ciencias-humanas?trimestre=${trimestre}`}
-        variant="primary"
-        text="Humanas"
-        type="button"
-        className="mt-6 w-full"
-      />
-    </div>
-  );
-}
-
-function GetNatureza({ trimestre }: { trimestre: string }) {
-  return (
-    <div className="rounded-2xl border border-aquamarine bg-space-cadet bg-topography-aquamarine p-6">
-      <Button
-        href={`/areas-conhecimento/ciencias-natureza?trimestre=${trimestre}`}
-        variant="primary"
-        text="Natureza"
-        type="button"
-        className="mt-6 w-full"
-      />
-    </div>
-  );
-}
-
-function GetLinguagens({ trimestre }: { trimestre: string }) {
-  return (
-    <div className="rounded-2xl border border-bright-pink bg-space-cadet bg-topography-bright-pink p-6">
-      <Button
-        href={`/areas-conhecimento/linguagens?trimestre=${trimestre}`}
-        variant="primary"
-        text="Linguagens"
-        type="button"
-        className="mt-6 w-full"
-      />
-    </div>
-  );
-}
-
-function GetMatematica({ trimestre }: { trimestre: string }) {
-  return (
-    <div className="rounded-2xl border border-sunglow bg-space-cadet bg-topography-sunglow p-6">
-      <Button
-        href={`/areas-conhecimento/matematica?trimestre=${trimestre}`}
-        variant="primary"
-        text="Matematica"
-        type="button"
-        className="mt-6 w-full"
-      />
-    </div>
-  );
-}
-
-function GetTecnologia({ trimestre }: { trimestre: string }) {
-  return (
-    <div className="rounded-2xl border border-bright-pink bg-space-cadet bg-topography-bright-pink p-6">
-      <Button
-        href={`/areas-conhecimento/tecnologia?trimestre=${trimestre}`}
-        variant="primary"
-        text="Tecnologia da Informação"
-        type="button"
-        className="mt-6 w-full"
-      />
-    </div>
-  );
-}
-
-function GetPPE({ trimestre }: { trimestre: string }) {
-  return (
-    <div className="rounded-2xl border border-aquamarine bg-space-cadet bg-topography-aquamarine p-6">
-      <Button
-        href={`/areas-conhecimento/ppe?trimestre=${trimestre}`}
-        variant="primary"
-        text="PPE"
-        type="button"
-        className="mt-6 w-full"
-      />
-    </div>
-  );
-}
+import { Suspense } from 'react';
+import AreaContent from '@/components/AreaContent';
 
 export default function AreasConhecimento() {
-  const params = useSearchParams();
-  const trimestre = params.get('trimestre') ?? '';
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Container className="mt-12 md:mt-14">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col space-y-10">
-            <GetHumanas trimestre={trimestre} />
-            <GetNatureza trimestre={trimestre} />
-            <GetTecnologia trimestre={trimestre} />
-          </div>
-          <div className="flex flex-col space-y-10">
-            <GetLinguagens trimestre={trimestre} />
-            <GetMatematica trimestre={trimestre} />
-            <GetPPE trimestre={trimestre} />
-          </div>
-        </div>
-      </Container>
+      <AreaContent />
     </Suspense>
   );
 }
